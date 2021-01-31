6c9581b9248b9f1868b7d356942044055c4071cf
Paul Applegate <paul@theapplegates.com>
2021-01-31 13:45:03 -0500

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-source-filesystem`,
+    `gatsby-transformer-sharp`,
+ ]
+}
---
+++ src/components/forest.js
@@ -0,0 +1,5 @@
+import { StaticImage } from "gatsby-plugin-image"
+
+export function forest() {
+  return <StaticImage src="/images/forest.jpg" alt="A dinosaur" />
+}
